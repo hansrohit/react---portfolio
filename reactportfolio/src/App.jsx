45add006f90560d055f19ab20b02b320ad45001e
Hans Rohit <hansrohit031@gmail.com>
2024-09-18 13:44:22 +0530

    nav commit 3.0

--- reactportfolio/src/App.jsx
+++ reactportfolio/src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Navbar from "./components/Navbar"
-import { Route,Routes } from 'react-router-dom'
+import { BrowserRouter, Route,Routes } from 'react-router-dom'
 import Project from './pages/Project'
 import Home from './pages/Home'
 import Contact from './pages/Contact'
@@ -8,12 +8,14 @@ import Contact from './pages/Contact'
 const App = () => {
   return (
     <>
-      <Navbar />
+    <BrowserRouter>
+    <Navbar/>
     <Routes>
       <Route path='/home' element={<Home/>}/>
       <Route path='/project' element={<Project/>}/>
       <Route path='/contact' element={<Contact/>}/>
     </Routes>
+    </BrowserRouter>
     </>
   )
 }
--- reactportfolio/src/components/Navbar.jsx
+++ reactportfolio/src/components/Navbar.jsx
@@ -2,16 +2,32 @@ import React from 'react'
 import {CircleUser} from 'lucide-react';
 import {Link} from 'react-router-dom'
 
-const Navbar = () => {
+const navbar = () => {
+
+   const navdata=[{
+    title:'Home',
+    path:'/home'
+   },{
+    title:'Project',
+    path:'/project'
+   },
+  {
+    title:'Contact',
+    path:'/contact'
+  }]
+   
   return (
+    <>
     <div className='bg-slate-300 rounded-sm h-[4rem] w-full shadow-xl bg-opacity-60 flex justify-around items-center sticky'>
           <div className='h-full w-[95%] flex justify-center items-center'>
             <div className='flex h-full w-full justify-start items-center '><h1 className='font-bold'><Link to='/home'>HANS ROHIT Y</Link></h1></div>
            <div className='flex justify-end items-center h-full w-[50%]'>
             <ul className='h-full w-full list-none flex flex-row justify-center gap-1 items-center font-semibold'>
-                <li className='border-1  justify-center flex items-center h-[100%] w-[40%] hover:bg-slate-800 hover:bg-opacity-30 hover:text-white'><Link to='/home'>Home</Link></li>
-                <li className='border-1 justify-center flex items-center h-[100%] w-[40%] hover:bg-slate-800 hover:text-white hover:bg-opacity-30'><Link to='/project'>Project</Link></li>
-                <li className='border-1 justify-center flex items-center h-[100%] w-[40%] hover:bg-slate-800 hover:text-white hover:bg-opacity-30'><Link to='/contact'>Contact</Link></li>
+            {navdata.map((data,index)=>(
+              <Link key={index} to={data.path} className='h-[100%] w-[40%]'><li className='border-1  justify-center flex items-center h-full w-full hover:bg-slate-800 hover:bg-opacity-30 hover:text-white'>{data.title}</li></Link>
+            ))}
+                {/* <li className='border-1 justify-center flex items-center h-[100%] w-[40%] hover:bg-slate-800 hover:text-white hover:bg-opacity-30'><Link to='/project'>Project</Link></li>
+                <li className='border-1 justify-center flex items-center h-[100%] w-[40%] hover:bg-slate-800 hover:text-white hover:bg-opacity-30'><Link to='/contact'>Contact</Link></li> */}
             <div className=' h-[100%] w-[18%] border-1 justify-center items-center flex'>
                 <Link to='/home'><li className='border-2 border-black rounded-full h-[32px] w-[32px] flex justify-start items-center'><CircleUser size={32} /></li></Link>
                 </div>
@@ -19,7 +35,8 @@ const Navbar = () => {
             </div>
        </div> 
      </div>
+     </>
   )
 }
 
-export default Navbar
+export default navbar
